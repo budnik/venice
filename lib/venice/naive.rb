b5936d2134cfdc1a3602c54ba5413ab95ceedf06
Dmytro Budnyk <dmitriy.budnik@gmail.com>
2014-11-02 00:16:43 +0200

    tiny refactorings

--- lib/venice/naive.rb
+++ lib/venice/naive.rb
@@ -1,18 +1,18 @@
 module Venice
   class Naive < Struct.new(:canals)
-    def solve(start, finish, stops: nil, max_stops: stops, max_cost: Float::INFINITY)
-      max_stops ||= stops || Float::INFINITY
+    def solve(start, finish, max_cost: Float::INFINITY, stops: nil, max_stops: stops||Float::INFINITY)
+      ok_length = (stops.to_i..max_stops)
 
       go = ->(dest, cost=0, route=dest) do
-        return [] if route.size > max_stops
+        if route.size < ok_length.last
+          routes_available = canals[dest].to_a.reject{|_, trip_cost| cost + trip_cost > max_cost}
 
-        routes_available = canals[dest].to_a.reject{|_, trip_cost| cost+trip_cost > max_cost}
-
-        routes_available.flat_map do |next_trip, trip_cost|
-          if next_trip == finish and not (stops and stops != route.size)
-            [route + next_trip]
-          end.to_a + go[next_trip, cost+trip_cost, route+next_trip]
-        end.compact
+          routes_available.flat_map do |next_trip, trip_cost|
+            if next_trip == finish and ok_length===route.size
+              [route + next_trip]
+            end.to_a + go[next_trip, cost+trip_cost, route+next_trip].to_a
+          end.compact
+        end
       end
 
       go[start]
